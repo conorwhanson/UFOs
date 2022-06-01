b065af12025093a180226b501288cf17c037fba8
Conor Hanson <condorhanson@gmail.com>
2022-06-01 13:53:41 -0500

    @ 11.3.1

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// import data from data.js file as a const for one-time raw data to be filtered later
+const tableData = data;
+
+// Reference the html table using D3; this tells JS to look for tbody tags in html script
+var tbody = d3.select("tbody");
