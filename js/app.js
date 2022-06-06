3eeb74ad431dd17d85fc6d552beb97a99495d26a
Conor Hanson <condorhanson@gmail.com>
2022-06-06 10:10:20 -0500

    @ 11.5.3

--- js/app.js
+++ js/app.js
@@ -4,3 +4,22 @@ const tableData = data;
 // Reference the html table using D3; this tells JS to look for tbody tags in html script
 var tbody = d3.select("tbody");
 
+// Function to create a table
+function buildTable(data) {
+
+    // clear table data
+    tbody.html("");
+
+    // loop through wach object in data and add a row and cells for each value in the object
+    data.forEach((dataRow) => {
+
+        // tell JS to look for <tbody> in html and append a table row ("tr")
+        let row = tbody.append("tr");
+
+        // loop through each iterable in dataRow and add values to table row; val represents each item in object (dict)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        }
+    );
+})};
